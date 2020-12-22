4eab1b771e2b816bff0d52644e57397aa8df3923
minexo79 <minexo79@gmail.com>
2020-12-22 11:44:31 +0800

    change example solve

---
+++ SZCP_PracticeS1/Question-Example-Solve/Q1-Triangle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void) {
+    int b;
+    cin >> b;
+    do {
+        cout << ((float)b*log2(b)) / 2 << '\n';
+        cin >> b;
+    } while(b != -1);
+    return 0;
+}
