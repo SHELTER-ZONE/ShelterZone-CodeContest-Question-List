a25d29d4a57a055c0f0babf61509bcda3c47cbb9
minexo79 <minexo79@gmail.com>
2020-12-24 18:57:18 +0800

    add 3 problems

--- SZCP_PracticeS1/Question-Example-Solve/Q6-3n+1.cpp
+++ SZCP_PracticeS1/Question-Example-Solve/Q6-3n+1.cpp
@@ -12,19 +12,37 @@ int retFunc(int src) {
         return retFunc((3*src) + 1);
 }
 
+void sort(int *src,int range) {
+    for(auto i = 0; i < range - 1; i++) {
+        for(auto j = i;j < range - 1; j++) {
+            if(src[j+1] < src[j]) {
+                int pt = src[j+1];
+                src[j+1] = src[j];
+                src[j] = pt;
+            }
+        }
+    }
+}
+
 int maxsolve(int i, int j) {
-    int max = 0; 
-    int mm,mi;
+    int mm,mi, c=0, ans;
     mi = (i > j) ? i : j;
     mm = (i <= j) ? i : j;
+    int * counter = (int *)calloc(((mi - mm)+1),sizeof(int));
     for(int k=mm;k<=mi;k++) {
         retFunc(k);
-        if(count > max)
-            max = count;
-        count = 0;
+        counter[c] = count;
+        count = 0; c++;
     }
-    return max;
+    sort(counter,((mi - mm)+1));
+    if((mm - mi+1) % 2)
+        ans = counter[((mi - mm)+1) / 2];
+    else 
+        ans = (counter[((mi - mm)+1) / 2] + counter[(((mi - mm)+1) / 2) - 1]) / 2;
+    free(counter);
+    return ans;
 }
+
 
 int main (void) {
     int i,j;
