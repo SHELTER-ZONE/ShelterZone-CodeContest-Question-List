94d968a0bcc915e7bc7ea5bfb19b044780f5b02d
minexo79 <minexo79@gmail.com>
2020-12-31 22:47:29 +0800

    udpate Q1 problem

--- SZCP_PracticeS1/Question-Example-Solve/Q1-Triangle.cpp
+++ SZCP_PracticeS1/Question-Example-Solve/Q1-Triangle.cpp
@@ -5,7 +5,7 @@ int main (void) {
     int b;
     cin >> b;
     do {
-        cout << ((float)b*log2(b)) / 2 << '\n';
+        cout << fixed << setprecision(7) << ((float)b*log2(b)) / 2 << '\n';
         cin >> b;
     } while(b != -1);
     return 0;
